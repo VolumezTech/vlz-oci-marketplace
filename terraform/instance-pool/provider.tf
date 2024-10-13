37177a156357ef10dc4e81ae518052ed76782066
Itay Ginor <itay.ginor@volumez.com>
2024-10-13 15:50:53 +0300

    update provider tf

---
+++ terraform/instance-pool/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "hashicorp/oci"
+      version = "6.4.0" # Specify the desired version or version constraint here
+    }
+    
+  }
+}
+provider "oci" {
+  auth = "SecurityToken"
+  config_file_profile = var.config_file_profile
+  region = var.region
+}
