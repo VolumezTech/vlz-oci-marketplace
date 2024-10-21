23379e1c7f50b61a9232730a22b1676c25c7748a
Itay Ginor <itay.ginor@volumez.com>
2024-10-21 19:29:10 +0300

    some fixes

--- terraform/instance-pool/outputs.tf
+++ terraform/instance-pool/outputs.tf
@@ -1,6 +1,6 @@
 output "tls_private_key" {
   value     = tls_private_key.ssh_key.private_key_pem
-  # sensitive = true
+  sensitive = true
 }
 # output "all-availability-domains-in-your-tenancy" {
 #   value = data.oci_identity_availability_domains.ads.availability_domains
