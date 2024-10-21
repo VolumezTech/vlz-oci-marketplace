9d23fc8bd64d58cd0f80d639b204f3bde9668812
Itay Ginor <itay.ginor@volumez.com>
2024-10-21 15:24:46 +0300

    some fixes

--- terraform/instance-pool/main.tf
+++ terraform/instance-pool/main.tf
@@ -1,13 +1,13 @@
-# provider "oci" {
-#   auth   = "InstancePrincipal"
-#   region = var.region  # Reference the region variable
-# }
-
 provider "oci" {
-  auth = "SecurityToken"
-  config_file_profile = var.config_file_profile
-  region = var.region
+  auth   = "InstancePrincipal"
+  region = var.region  # Reference the region variable
 }
+
+# provider "oci" {
+#   auth = "SecurityToken"
+#   config_file_profile = var.config_file_profile
+#   region = var.region
+# }
 
 resource "null_resource" "singup_user" {
   provisioner "local-exec" {
