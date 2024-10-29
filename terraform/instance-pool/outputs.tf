c5332787eb7141c7c2340728c06c0e26ed96797b
Itay Ginor <itay.ginor@volumez.com>
2024-10-29 15:37:45 +0200

    fix description

--- terraform/instance-pool/outputs.tf
+++ terraform/instance-pool/outputs.tf
@@ -2,9 +2,6 @@ output "tls_private_key" {
   value     = tls_private_key.ssh_key.private_key_pem
   sensitive = true
 }
-# output "all-availability-domains-in-your-tenancy" {
-#   value = data.oci_identity_availability_domains.ads.availability_domains
-# }
 output "public-ip-media-instance-pool" {
   value = { for instance in data.oci_core_instance.media_instance : instance.id => instance.public_ip}
 }
