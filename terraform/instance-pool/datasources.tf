3107d76a5b0818e204e2d5687c4160052f1201c0
Itay Ginor <itay.ginor@volumez.com>
2024-10-30 16:17:02 +0200

    debug

--- terraform/instance-pool/datasources.tf
+++ terraform/instance-pool/datasources.tf
@@ -54,29 +54,6 @@ data "oci_core_instance" "app_instance" {
   instance_id = data.oci_core_instance_pool_instances.app_pool[0].instances[count.index].id
 }
 
-data "oci_core_vnic_attachments" "app_vnic2_attachments" {
-  count = local.secondary_vnic_config
-
-  compartment_id = var.tenancy_ocid
-  instance_id    = data.oci_core_instance_pool_instances.app_pool[count.index].instances[0].id
-
-  filter {
-    name   = "subnet_id"
-    values = [oci_core_subnet.vlz_subnet[1].id]
-  }
-}
-
-#VNIC object by OCID
-data "oci_core_vnic" "app_vnic2_id" {
-  count = local.secondary_vnic_config
-
-  vnic_id = lookup(data.oci_core_vnic_attachments.app_vnic2_attachments[count.index].vnic_attachments[0], "vnic_id")
-}
-
-data "oci_core_private_ips" "app_vnic2_ip" {
-  count   = local.secondary_vnic_config
-  vnic_id = data.oci_core_vnic.app_vnic2_id[count.index].id
-}
 
 ### Media Data ###
 
