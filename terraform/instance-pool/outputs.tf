e83a730dfadfc748d9955d6b23301838734266d8
Itay Ginor <itay.ginor@volumez.com>
2024-10-31 12:30:12 +0200

    remove debug

--- terraform/instance-pool/outputs.tf
+++ terraform/instance-pool/outputs.tf
@@ -1,13 +1,7 @@
 output "tls_private_key" {
-  # value     = var.generate_public_ssh_key ? tls_private_key.public_private_key_pair.private_key_pem : "Use your own private key"
-  value     = tls_private_key.public_private_key_pair.private_key_pem 
+  value     = var.generate_public_ssh_key ? tls_private_key.public_private_key_pair.private_key_pem : "Use your own private key"
   sensitive = true
 }
-
-output "tls_public_key" {
-  value = tls_private_key.public_private_key_pair.public_key_openssh
-}
-
 output "public-ip-media-instances" {
   value = [{ for instance in data.oci_core_instance.media_instance : instance.display_name => instance.public_ip}]
 }
