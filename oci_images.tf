87c1d58578031ce38e12fac4a1d0316ec0fa51d7
VolumezTech <102028115+VolumezTech@users.noreply.github.com>
2024-12-04 17:17:14 +0200

    Update oci_images.tf - ocid src oci-hpc gh repo

--- oci_images.tf
+++ oci_images.tf
@@ -1,2 +1,14 @@
 variable "marketplace_source_images" {
+  type = map(object({
+    ocid                  = string
+    is_pricing_associated = bool
+    compatible_shapes     = set(string)
+  }))
+  default = {
+    main_mktpl_image = {
+      ocid                  = "ocid1.image.oc1..aaaaaaaa5yxem7wzie34hi5km4qm2t754tsfxrjuefyjivebrxjad4jcj5oa"
+      is_pricing_associated = false
+      compatible_shapes     = []
+    }
+  }
 }
