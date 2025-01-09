ae58db61df46f0a128990fa7a36d0a806d4d7648
Itay Ginor <itay.ginor@volumez.com>
2025-01-09 12:08:41 +0200

    some fixes

--- scripts/vlz_api.py
+++ scripts/vlz_api.py
@@ -208,7 +208,7 @@ def main():
             "name": vol_name,
             "type": "file",
             "size": 2500 if env_size == "Small" else 6000 if env_size == "Medium" else 10000,
-            "policy": env_size,
+            "policy": env_size.lower(),
             "zone": zone,   
         }
         create_volume(OCI_URL, token, volume_body)
