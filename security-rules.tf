50ac704ea854d4979ee5095f3b17b1cbfffd4a89
Itay Ginor <itay.ginor@volumez.com>
2025-01-26 15:46:57 +0200

    some fixes

--- security-rules.tf
+++ security-rules.tf
@@ -1,21 +1,3 @@
-# resource "oci_core_security_list" "volumez_sl" {
-#   compartment_id = var.compartment_ocid
-#   vcn_id         = oci_core_vcn.vlz_vcn.id
-#   display_name   = "volumez-sl-${random_string.deploy_id.result}"
-
-#   egress_security_rules {
-#     protocol         = "all"
-#     destination      = "0.0.0.0/0"
-#     destination_type = "CIDR_BLOCK"
-#   }§
-
-#   ingress_security_rules {
-#     protocol    = "all"
-#     source      = "0.0.0.0/0"
-#     source_type = "CIDR_BLOCK"
-#   }
-# }
-
 resource "oci_core_security_list" "volumez_sl" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_vcn.vlz_vcn.id
